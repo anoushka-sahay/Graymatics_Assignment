0685ec7462b791ab48a45aba53834de33344f501
Anoushka Sahay <104036893+anoushka-sahay@users.noreply.github.com>
2023-06-06 01:35:51 +0530

    Update Problem_2.py

--- Problem_2.py
+++ Problem_2.py
@@ -1,5 +1,15 @@
 def problem_2(str):
     stack = []
+    
+    # edge case if number of opening bracket is not equal to number of closing bracket then return False
+    if str.count('(') != str.count(')'):
+        return False
+    if str.count('{') != str.count('}'):
+        return False
+    if str.count('[') != str.count(']'):
+        return False
+    
+    #adding opening bracket in str to the stack. If we encounter a closing bracket then we remove the opening bracket of same kind from the stack
     for x in range(len(str)):
         if str[x] == '(' or str[x] == '{' or str[x] == '[':
             stack.append(str[x])
@@ -12,6 +22,8 @@ def problem_2(str):
         if str[x] == "]":
             if len(stack) != 0 and stack[len(stack)-1] == "[":
                 stack.pop()
+                
+    #if string is valid then its size will be zero because for each closing bracket we removed the opening bracket
     if len(stack) == 0:
         return True
     else:
